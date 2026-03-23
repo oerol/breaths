24beb8fd277a73a64f65147d222506950c0f78f3
Özgür Erol <erolxerolx@gmail.com>
2026-03-23 17:52:03 +0100

    refactor: move styles to global buttons.css

--- src/components/StartBreathingButton.tsx
+++ src/components/StartBreathingButton.tsx
@@ -1,12 +1,14 @@
-import "./StartBreathingButton.css";
-
 export default function StartBreathingButton({
   setIsBreathing,
 }: {
   setIsBreathing: (value: boolean) => void;
 }) {
   return (
-    <button id="start-breathing-button" onClick={() => setIsBreathing(true)}>
+    <button
+      id="start-breathing-button"
+      className="button"
+      onClick={() => setIsBreathing(true)}
+    >
       start
     </button>
   );
