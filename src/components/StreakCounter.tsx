b8640ea873d7e7ccd9c9ddbe917b11d0707f59e9
Özgür Erol <erolxerolx@gmail.com>
2026-04-08 14:20:29 +0200

    feat: animate StreakCounter

--- src/components/StreakCounter.tsx
+++ src/components/StreakCounter.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, type AnimationEvent } from "react";
 import type { StreakCounter } from "../services/streak-counter/streak-counter.interface";
 import "./StreakCounter.css";
 import { FlameIcon } from "../assets/icons/Flame";
@@ -11,18 +11,44 @@ export default function StreakCounter({
   streakCounter: StreakCounter;
 }) {
   const [streakCount, setStreakCount] = useState(streakCounter.getCount());
+  const [className, setClassName] = useState<string | undefined>(undefined);
+
+  const startFirstHalfOfNumberAnimation = () => {
+    setClassName("vanish");
+  };
+
+  const startSecondHalfOfNumberAnimation = () => {
+    setClassName("appear");
+  };
 
   useEffect(() => {
     if (isFinished) {
+      // TODO: Return the updated value here.
       streakCounter.addToStreak();
-      setStreakCount(streakCounter.getCount());
+
+      const updatedCount = streakCounter.getCount();
+      const streakCountHasIncreased = streakCount !== updatedCount;
+
+      if (streakCountHasIncreased) {
+        startFirstHalfOfNumberAnimation();
+      }
     }
   }, [isFinished]);
+
+  const onAnimationEnd = (event: AnimationEvent) => {
+    //TODO: Get rid of this magic string.
+    if (event.animationName === "number-vanish") {
+      setStreakCount(streakCounter.getCount());
+      startSecondHalfOfNumberAnimation();
+    }
+  };
 
   return (
     <div id="streak-counter">
       <FlameIcon />
-      {streakCount}
+      <span className={className} id="count" onAnimationEnd={onAnimationEnd}>
+        {streakCount}
+      </span>
     </div>
   );
 }
